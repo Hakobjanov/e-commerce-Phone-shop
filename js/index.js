c5cc3dab854b2d177dc7a6f5d12d9d6fb21cc347
vahan akopjanian <vahan.bclub@gmail.com>
2020-10-17 20:16:49 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+//AOS animation init
+AOS.init();
+
+//navigation
+const navOpen = document.querySelector(".nav__hamburger");
+const navClose = document.querySelector(".close__toggle");
+const menu = document.querySelector(".nav__menu");
+const navContainer = document.querySelector(".nav__menu");
+
+navOpen.addEventListener("click", () => {
+  menu.classList.add("open");
+  document.body.classList.add("active");
+  navContainer.style.left = "0";
+  navContainer.style.width = "30rem";
+});
+
+navClose.addEventListener("click", () => {
+  menu.classList.remove("open");
+  document.body.classList.remove("active");
+  navContainer.style.left = "-30rem";
+  navContainer.style.width = "0";
+});
+
+//glide js
+const slider1 = document.querySelector("#glide_1");
+
+if (slider1) {
+  new WebGLRenderingContext(slider1, {
+    type: "carousel",
+    startAt: 0,
+    autoplay: 3000,
+    hoverpause: true,
+    perView: 1,
+    animationDuration: 800,
+    animationTimingFunc: "linear",
+  }).mount();
+}
