007421b34b6b881f031d7a6271dbe7d6b723631e
vahan akopjanian <vahan.bclub@gmail.com>
2020-10-17 21:06:26 +0300

    slider with styles added

--- js/index.js
+++ js/index.js
@@ -25,10 +25,10 @@ navClose.addEventListener("click", () => {
 const slider1 = document.querySelector("#glide_1");
 
 if (slider1) {
-  new WebGLRenderingContext(slider1, {
+  new Glide(slider1, {
     type: "carousel",
     startAt: 0,
-    autoplay: 3000,
+    // autoplay: 3000,
     hoverpause: true,
     perView: 1,
     animationDuration: 800,
