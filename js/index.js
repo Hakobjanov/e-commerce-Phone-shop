b7b4263810f67a54069d776205f2a42701480e70
vahan akopjanian <vahan.bclub@gmail.com>
2020-10-20 16:44:40 +0300

    added popup with styles

--- js/index.js
+++ js/index.js
@@ -56,3 +56,64 @@ if (slider2) {
     },
   }).mount();
 }
+
+//PopUp
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup__close");
+
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide__popup");
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide__popup");
+    }, 500);
+  });
+}
+
+//Fixed Navigation
+const navBar = document.querySelector(".navigation");
+const gotoTop = document.querySelector(".goto-top");
+const scrollLink = document.querySelectorAll(".scroll-link");
+
+Array.from(scrollLink).map((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navBar.getBoundingClientRect().height;
+    const fixNav = navBar.classList.contains("fix__nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixNav) {
+      position = position - navHeight;
+    }
+
+    window.scrollTo({
+      lwft: 0,
+      top: position,
+    });
+    navContainer.style.left = "-30rem";
+    document.body.classList.remove("active");
+  });
+});
+
+window.addEventListener("scroll", (e) => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navBar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix__nav");
+  } else {
+    navBar.classList.remove("fix__nav");
+  }
+
+  if (scrollHeight > 300) {
+    gotoTop.classList.add("show-top");
+  } else {
+    gotoTop.classList.rmeove("show-top");
+  }
+});
